6be38fb7fb8badb2edfcedf222b7ba70c8d9732b
VindhyaKulasinghe <vindyakulasinghe@gmail.com>
2025-03-08 12:02:07 +0530

    Signup2 update

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Login from './pages/Login'
+import signup2 from './pages/signup2'
 function App() {
   return (
     <>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/signup2" element={<signup2/>}/>
       </Routes>
      </BrowserRouter>
     </>
---
+++ Project1/src/pages/signup2.jsx
@@ -0,0 +1,6 @@
+function signup2() {
+  return (<><h1>Signup2</h1></>
+
+  );
+}
+export default signup2;
